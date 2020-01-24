24c8a81239365f1f114c03663c2199cafc6218e7
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-23 17:13:06 -0800

    post meddleware added

--- users/userRouter.js
+++ users/userRouter.js
@@ -16,7 +16,7 @@ router.post('/',validateUser, (req, res) => {
   })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts',validatePost, (req, res) => {
   // do your magic!
 });
 
@@ -80,7 +80,16 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body || req.body.length<1){
+    res.status(400).json({message: "missing post data"})
+  } else if(!req.body.text || req.body.text.length<1) {
+    res.status(400).json({message: "missing required text field"})
+  } else{
+    next();
+  }
 }
+
+
 
 function errorHandler(error, req, res, next) {
   console.log('error: ', error.message);
